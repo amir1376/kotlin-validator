9767cd8bf28716f41bcecabe17e83fa923579bf7
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2023-02-02 20:06:18 +0330

    fix startsWithEnglishCharacter bug in regex

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 allprojects {
    group = "ir.amirab.validator"
-   version = "0.0.1-beta.2"
+   version = "0.0.1-beta.3"
    repositories {
       google()
       mavenCentral()
--- core/src/main/java/ir/amirab/validator/rule/DependentInput.kt
+++ core/src/main/java/ir/amirab/validator/rule/DependentInput.kt
@@ -4,7 +4,7 @@ import kotlin.reflect.KProperty1
 
 data class DependentInput<C, out T>(
     val receiver: C,
-    val prop: KProperty1<C, T>,
+    val prop: (C)-> T,
 ) {
-    val actualInput get():T = prop.get(receiver)
+    val actualInput get():T = prop(receiver)
 }
